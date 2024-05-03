553287374f85a6e901e97e13f03e692242c70c72
BorisMiroAtanasov <borisatanasov84@abv.bg>
2024-05-04 00:11:11 +0300

    add new comment to local state

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -29,6 +29,7 @@ export default function GameDetails(){
             formData.get('comment')
 
         )
+        setComments(state => [...state, newCommnet])
         console.log(newCommnet);
     }
     return (
@@ -52,7 +53,7 @@ export default function GameDetails(){
                 <h2>Comments:</h2>
                 <ul>
                     {/* <!-- list all comments for current game (If any) --> */}
-                    {comments.map( ({username , text}) =>  <li className="comment">
+                    {comments.map( ({_id, username , text}) =>  <li key={_id} className="comment">
                         <p>{username}: {text}</p>
                     </li>)}
                    
